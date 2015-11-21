637d2b04196c0c796a7d44117b466f98ab987eb5
sagidana1 <sagidana1@gmail.com>
2015-11-21 23:00:52 +0200

    pcap ready

---
+++ ReactionTime/NetworkManager.h
@@ -0,0 +1,13 @@
+#include "pcap.h"
+
+#pragma comment(lib , "ws2_32.lib")
+#pragma comment(lib , "wpcap.lib")
+
+#pragma once
+class NetworkManager
+{
+public:
+	NetworkManager();
+	~NetworkManager();
+};
+
